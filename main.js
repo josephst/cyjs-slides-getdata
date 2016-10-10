505b74dae679f5c6f348d98cc96ce32a50db9978
Joseph Stahl <josephst18+git@outlook.com>
2016-10-10 17:00:50 -0500

    v 0.1 data generation working

--- main.js
+++ main.js
@@ -1,105 +1,80 @@
-let Twit = require('twit');
-let fs = require('fs');
-let moment = require('moment');
 let _ = require('lodash');
+let fs = require('fs');
 
-var T = new Twit({
-  consumer_key: 'CzriYfPsn1NeZjnfVHp8ZQSl1',
-  consumer_secret: 'A5lYgc0j90lqJKA2soNLR6X3g8ZBAYm1O32pG8LusO4IkREO76',
-  app_only_auth: true
-});
+let NODECOUNT = 25;
+let DAYSTOFILL = 30;
+let EDGEFACTOR = 3;
 
-let users = {};
-
-let getTweetsByDay = function(screen_name) {
-  T.get('statuses/user_timeline', { screen_name: screen_name })
-}
-
-let finishedGettingUserList = function(result) {
-  let users = result.data.users;
-  let userTweets = [];
-  let twitPromises = [];
-  users.forEach(user => {
-    twitPromises.push(T.get('statuses/user_timeline', { screen_name: user.screen_name, count: 200 }));
-  });
-  return Promise.all(twitPromises)
-    .then(function(timelineData) {
-      timelineData.forEach(timeline => {
-        let timelineData = timeline.data;
-        let screen_name = timelineData[0].user.screen_name;
-        let tweets = [];
-        timelineData.forEach(tweet => tweets.push({
-          'date': tweet.created_at,
-          'text': tweet.text
-        }));
-        userTweets.push({
-          'screen_name': screen_name,
-          'tweets': tweets
-        });
-      });
-      return userTweets;
-    });
-}
-
-let processTweetsByDate = function(userTweets) {
-  let daysAgo = _.range(0, 30);
-  let userTweetsByDay = [];
-  return userTweets.map(function(user) {
-    let tweets = user.tweets;
-    let tweetsByDay = daysAgo.map(function(numberOfDaysAgo) {
-      let targetDay = moment().subtract(numberOfDaysAgo, 'days');
-      let tweetsOnDay = _.filter(tweets, tweet => {
-        return moment().utc(tweet.date).isSame(targetDay, 'day')
-      })
-      .map(tweet => tweet.text)
-      return {
-        'day': targetDay.toISOString(),
-        'count': tweetsOnDay.length 
-      }
-    });
-    return {
-      'screen_name': user.screen_name,
-      'tweets_by_day': tweetsByDay,
+function generateNodes(count) {
+  let growthStrategies = {
+    genGrowingArr: function() {
+      let randomFactor = _.random(1, 3);
+      return _.range(1, DAYSTOFILL + 1).map(val => val * randomFactor);
+    },
+    genDiminishingArr: function() {
+      let randomFactor = _.random(1, 3);
+      return _.range(DAYSTOFILL, 0, -1).map(val => val * randomFactor);
+    },
+    genConstantArr: function() {
+      return _.fill(Array(DAYSTOFILL), _.random(10, 20));
     }
-  });
-}
-
-let process2 = function(userArr) {
-  let daysAgo = _.range(0, 30);
-  return userArr.map(user => {
-    let userName = user.screen_name;
-    let userTweetsByDate = daysAgo.map(function(numberOfDaysAgo) {
-      let targetDay = moment().subtract(numberOfDaysAgo, 'days');
-      let tweetsOnDay = _.filter(user.tweets, tweet => {
-        return moment.utc(tweet.date).local().isSame(targetDay, 'day')
-      });
-      return {
-        'day': numberOfDaysAgo,
-        'count': tweetsOnDay.length
-      }
-    });
-    return {
-      'user': userName,
-      'tweetsByDay': userTweetsByDate
-    }
-  });
-}
-
-// for getting fresh data
-
-// T.get('friends/list', { screen_name: 'nytimes' })
-//   .then(finishedGettingUserList)
-//   // .then(data => fs.writeFile('output.json', JSON.stringify(data, null, 2)));
-//   .then(process2)
-//   .then(data => fs.writeFile('output2.json', JSON.stringify(data, null, 2)));
-
-
-// for using cached data
-
-fs.readFile('output.json', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile('output2.json', JSON.stringify(process2(JSON.parse(data))));
   }
-});
+  let randomGrowthStrategy = function() {
+    let strategies = _.keys(growthStrategies);
+    return growthStrategies[strategies[_.random(strategies.length - 1)]]
+  }
+  let nodes = [];
+  _.range(0, count).forEach(val => {
+    let strategy = randomGrowthStrategy();
+    nodes.push({
+      data: {
+        id: val,
+        postsByDay: strategy(),
+        peopleFollowedCount: _.random(1, Math.floor(count/2))
+      }
+    });
+  })
+  return nodes;
+}
+
+function generateEdges(nodes) {
+  let edgeId = 0;
+  let edges = [];
+  let generateEdge = function(id, source, target) {
+    return {
+      data: {
+        id: id,
+        source: source,
+        target: target
+      }
+    };
+  }
+  nodes.forEach(node => {
+    let edgesToMake = node.data.peopleFollowedCount;
+    let possibleTargets = _.filter(nodes, target => {
+      return target.data.id != node.data.id; 
+    });
+    let currentNodeEdges = [];
+    for (let i = 0; i < node.data.peopleFollowedCount; i++) {
+      let target = possibleTargets[_.random(possibleTargets.length - 1)];
+      currentNodeEdges.push(generateEdge(edgeId++, node.data.id, target.data.id));
+      // then remove this node from further consideration (don't want 2 edges from this node to target)
+      _.remove(possibleTargets, possibleTarget => {
+        possibleTarget.data.id == target.data.id
+      });
+    }
+    edges = edges.concat(currentNodeEdges);
+  });
+  return edges;
+}
+
+function generateData(count) {
+  let nodes = generateNodes(count);
+  let edges = generateEdges(nodes);
+  console.log('done');
+  return {
+    elements: _.concat(nodes, edges)
+  }
+}
+
+fs.writeFile('output.json', JSON.stringify(generateData(NODECOUNT), null, 2));
