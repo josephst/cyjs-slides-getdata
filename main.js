952850c2c7483abaf129aac16dce23b0ba553bd0
Joseph Stahl <josephst18+git@outlook.com>
2016-10-10 19:38:54 -0500

    documentation

--- main.js
+++ main.js
@@ -5,20 +5,51 @@ let NODECOUNT = 25;
 let DAYSTOFILL = 30;
 let EDGEFACTOR = 3;
 
+/**
+ * Returns an array of nodes in Cytoscape.js format.
+ * The nodes will have an id, an array of counts corresponding to simulated social media posts,
+ * and a randomly selected number between 1 and (count / 2) to use when making edges from the node.
+ * 
+ * @param {Number} count The number of nodes to generate
+ * @return {Array} An array of objects to be added to Cytoscape.js
+ */
 function generateNodes(count) {
   let growthStrategies = {
+    /**
+     * Return an array of increasing integers from 1 to DAYSTOFILL + 1 with some random growth factor
+     * 
+     * @return Increasing array
+     */
     genGrowingArr: function() {
       let randomFactor = _.random(1, 3);
       return _.range(1, DAYSTOFILL + 1).map(val => val * randomFactor);
     },
+
+    /**
+     * Return an array of decreasing integers from 1 to DAYSTOFILL + 1 with some random growth factor
+     * 
+     * @return Decreasing array
+     */
     genDiminishingArr: function() {
       let randomFactor = _.random(1, 3);
       return _.range(DAYSTOFILL, 0, -1).map(val => val * randomFactor);
     },
+
+    /**
+     * Return an array of size DAYSTOFILL, filled with an integer between 10 and 20.
+     * 
+     * @return Constant array
+     */
     genConstantArr: function() {
       return _.fill(Array(DAYSTOFILL), _.random(10, 20));
     }
   }
+
+  /**
+   * Choose a random growth strategy from the growthStrategies object
+   * 
+   * @return A randomly selected function which will generate an array when called
+   */
   let randomGrowthStrategy = function() {
     let strategies = _.keys(growthStrategies);
     return growthStrategies[strategies[_.random(strategies.length - 1)]]
@@ -37,9 +68,26 @@ function generateNodes(count) {
   return nodes;
 }
 
+/**
+ * Returns an array of edges generated from an array of nodes.
+ * Each node's peopleFollowedCount is used to determine how many edges to add from the node.
+ * No node will have two edges to the same target node.
+ * No node will have any edge to itself.
+ * 
+ * @param {Array} nodes The array of nodes which will be used to get ids for the source and targets of edges.
+ * @return {Array} Edges (in Cytoscape.js JSON format) with an id, source, and target.
+ */
 function generateEdges(nodes) {
   let edgeId = 0;
   let edges = [];
+
+  /** Simple generator for Cytoscape.js-formatted JSON objects
+   * 
+   * @param {Number} id The ID of the edge
+   * @param {Number} source The ID of the source node
+   * @param {Number} target The ID of the target of the edge
+   * @return {Object} An object with a data property, containing the passed id, source, and target properties
+   */
   let generateEdge = function(id, source, target) {
     return {
       data: {
@@ -49,6 +97,7 @@ function generateEdges(nodes) {
       }
     };
   }
+
   nodes.forEach(node => {
     let edgesToMake = node.data.peopleFollowedCount;
     let possibleTargets = _.filter(nodes, target => {
@@ -68,6 +117,12 @@ function generateEdges(nodes) {
   return edges;
 }
 
+/**
+ * Create an array of data that can be used for Cytoscape.js JSON elements
+ * 
+ * @param {Number} count Number of nodes to generate
+ * @return {Array} An array of elements, both nodes and edges, which can be added to a Cytoscape.js graph
+ */
 function generateData(count) {
   let nodes = generateNodes(count);
   let edges = generateEdges(nodes);
